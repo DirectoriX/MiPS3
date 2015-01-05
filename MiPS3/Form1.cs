d244e42e2f38b1f70739b5210d53d8fc8129f751
DirectoriX <dontsov.mih@yandex.ru>
2015-01-06 00:45:37 +0300

    Убраны лишние using'и

--- MiPS3/Form1.cs
+++ MiPS3/Form1.cs
@@ -1,13 +1,8 @@
-﻿using System;
+﻿using Meta.Numerics.Statistics.Distributions;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Meta.Numerics.Statistics.Distributions;
 
 namespace MiPS3
 {
